41cdd87e727f00373715edef610bc79bf83a3703
Fabian Beltran <fabianbeltran1224@gmail.com>
2023-11-21 17:49:17 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
+<script setup>
+import SideBar from './components/SideBar.vue'
+</script>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar id="sidebar" classmsg="Welcome to Your Vue.js App" />
+    <SideBar id="header" classmsg="Welcome to Your Vue.js App" />
+    <SideBar id="main" classmsg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+* {
   color: #2c3e50;
-  margin-top: 60px;
+  border: 0px solid red;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+</style>
+
+<style scoped>
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 1fr 3fr;
+  grid-template-areas:
+    "sidebar header"
+    "sidebar main"
+}
+
+#sidebar {
+  grid-area: sidebar;
+}
+
+#header {
+  grid-area: header;
+}
+
+#main {
+  grid-area: main;
 }
 </style>
