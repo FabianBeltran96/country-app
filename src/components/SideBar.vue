f7a94a5fe4d37463efc66c2863f49d63e753b151
Fabian Beltran <fabianbeltran1224@gmail.com>
2023-11-21 17:49:43 -0500

    new sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  msg: String
+})
+
+console.log('hello script setup')
+</script>
+
+<template>
+  <div class="hello">
+    {{ props.msg }}
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  color: red;
+}
+
+</style>
